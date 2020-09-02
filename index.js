4e4ff434d78fd00108b674306882efd2f608bc0a
arnest00 <53844657+arnest00@users.noreply.github.com>
2020-09-02 16:59:24 -0400

    Add read, update, and delete operations

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const { mongoURI } = require('./config.json');
 
 const app = express();
 
+app.set('view engine', 'ejs'); // insert before any app.use, app.get, app.post methods
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 MongoClient.connect(mongoURI, { useUnifiedTopology: true })
   .then(client => {
@@ -13,18 +16,58 @@ MongoClient.connect(mongoURI, { useUnifiedTopology: true })
 
     const db = client.db('spellbook');
     const customSpellCollection = db.collection('custom-spells');
-
-    app.get('/', (req, res) => {
-      res.sendFile(__dirname + '/index.html');
-    });
     
+    // ====== CREATE
     app.post('/custom-spells', (req, res) => {
       customSpellCollection.insertOne(req.body)
         .then(result => {
           res.redirect('/');
         })
         .catch(error => console.error(error));
-    })
+    });
+
+    // ====== READ
+    app.get('/', (req, res) => {
+      customSpellCollection.find().toArray()
+        .then(results => {
+          res.render('index.ejs', { customSpells: results })
+        })
+        .catch(error => console.error(error));
+    });
+
+    // ====== UPDATE
+    app.put('/custom-spells', (req, res) => {
+      customSpellCollection.findOneAndUpdate(
+        { name: { $regex: /^((?!Magic Missile).)*$/ } },
+        {
+          $set: {
+            name: req.body.name,
+            desc: req.body.desc
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+        .then(result => {
+          res.json('Success!');
+        })
+        .catch(error => console.error(error));
+    });
+
+    // ====== DELETE
+    app.delete('/custom-spells', (req, res) => {
+      customSpellCollection.deleteOne(
+        { name: req.body.name }
+      )
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No spell to delete');
+          }
+          res.json('Deleted magic missile spell');
+        })
+        .catch(error => console.error(error));
+    });
     
     app.listen(3000, () => {
       console.log('Listening on port 3000!');
---
+++ public/main.js
@@ -0,0 +1,42 @@
+const updateBtn = document.querySelector('#update-button');
+const deleteBtn = document.querySelector('#delete-button');
+const messageDiv = document.querySelector('#message');
+
+updateBtn.addEventListener('click', e => {
+  fetch('/custom-spells', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      name: 'Magic Missile',
+      desc: 'pew pew pew'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json();
+    })
+    .then(response => {
+      window.location.reload(true);
+    })
+    .catch(error => console.error(error));
+});
+
+deleteBtn.addEventListener('click', e => {
+  fetch('/custom-spells', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      name: 'Magic Missile'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json();
+    })
+    .then(response => {
+      if (response === 'No spell to delete') {
+        messageDiv.textContent = 'No spell to delete.'
+      } else {
+        window.location.reload(true);
+      };
+    })
+    .catch(error => console.error(error));
+})
