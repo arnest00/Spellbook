268ea7b4dc70208a0a7d33b72db086589f3d6bb2
arnest00 <53844657+arnest00@users.noreply.github.com>
2020-10-15 14:28:50 -0400

    Add functionality to register users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 // ====== User model
 const User = require('../models/User');
@@ -12,6 +13,72 @@ router.get('/login', (req, res) => {
 // ====== Register page
 router.get('/register', (req, res) => {
   res.render('./users/register');
+});
+
+// ====== Register handle
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all fields' });
+  };
+  
+  // Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  };
+
+  // Check password length
+  if (password.length < 6) {
+    errors.push({ msg: 'Password should be at least 6 characters long'});
+  };
+
+  if (errors.length > 0) {
+    res.render('./users/register', {
+      errors,
+      name,
+      email
+    });
+  } else {
+    // Validation passed
+    User.findOne({ email: email })
+      .then(user => {
+        errors.push({ msg: 'E-mail is already in use' });
+        if (user) {
+          // User exists
+          res.render('./users/register', {
+            errors,
+            name,
+            email
+          });
+        } else {
+          const newUser = new User({
+            name,
+            email,
+            password
+          });
+
+          // Hash password
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if (err) throw err;
+
+              // Set password to hashed password
+              newUser.password = hash;
+              
+              // Save user
+              newUser.save()
+                .then(user => {
+                  res.redirect('/users/login');
+                })
+                .catch(err => console.log(err));
+            });
+          });
+        };
+      });
+  };
 });
 
 module.exports = router;
