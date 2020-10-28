6be4dd3b091387aa7b6ef52a578a0f5df81efa32
arnest00 <53844657+arnest00@users.noreply.github.com>
2020-10-28 16:08:11 -0400

    Create route to handle adding searched spell to spellbook

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,7 @@ const express = require('express'),
       fetch = require("node-fetch"),
       { ensureAuthenticated } = require('../middleware');
 
+const { create } = require('../models/Spell');
 // ====== Spell model
 const Spell = require('../models/Spell');
 
@@ -52,22 +53,61 @@ router.get('/:spell', ensureAuthenticated, async (req, res) => {
 
   const response = await fetch(`${url}${queryStr}`);
   const data = await response.json();
-  const foundSpell = {
-    name: data.results[0].name,
-    higherLevel: data.results[0].higher_level,
-    range: data.results[0].range,
-    material: data.results[0].material.toLowerCase().slice(0,-1),
-    duration: data.results[0].duration.toLowerCase(),
-    concentration: data.results[0].concentration,
-    time: data.results[0].casting_time,
-    school: data.results[0].school.toLowerCase()
-  };
-  foundSpell.desc = data.results[0].desc.split('\n');
-  foundSpell.components = data.results[0].components.toLowerCase().split(', ');
-  foundSpell.concentration = data.results[0].concentration === 'yes' ? 'on' : null;
-  foundSpell.level = data.results[0].level === 'Cantrip' ? '0th' : data.results[0].level.slice(0,3);
+  // const foundSpell = {
+  //   name: data.results[0].name,
+  //   higherLevel: data.results[0].higher_level,
+  //   range: data.results[0].range,
+  //   material: data.results[0].material.toLowerCase().slice(0,-1),
+  //   duration: data.results[0].duration.toLowerCase(),
+  //   concentration: data.results[0].concentration,
+  //   time: data.results[0].casting_time,
+  //   school: data.results[0].school.toLowerCase()
+  // };
+  // foundSpell.desc = data.results[0].desc.split('\n');
+  // foundSpell.components = data.results[0].components.toLowerCase().split(', ');
+  // foundSpell.concentration = data.results[0].concentration === 'yes' ? 'on' : null;
+  // foundSpell.level = data.results[0].level === 'Cantrip' ? '0th' : data.results[0].level.slice(0,3);
+  const foundSpell = createSpellObj(data.results[0]);
 
   res.render('./search/spell.ejs', { foundSpell: foundSpell });
 });
+
+// ====== Add chosen spell to spellbook
+router.post('/:spell', ensureAuthenticated, async (req, res) => {
+  let queryStr = `slug=${ req.params.spell }`;
+
+  const response = await fetch(`${url}${queryStr}`);
+  const data = await response.json();
+  const newSpell = createSpellObj(data.results[0]);
+  newSpell.author = {
+    id: req.user.id,
+    name: req.user.name
+  };
+
+  Spell.create(newSpell, err => {
+    if (err) throw err;
+
+    res.redirect('/spells');
+  });
+});
+
+// ====== Extract relevant data
+const createSpellObj = data => {
+  return {
+    name: data.name,
+    level: data.level === 'Cantrip' ? '0th' : data.level.slice(0,3),
+    higherLevel: data.higher_level,
+    desc: data.desc,
+    range: data.range,
+    components: data.components.toLowerCase().split(', '),
+    material: data.material.toLowerCase().slice(0,-1),
+    concentration: data.concentration === 'yes' ? 'on' : null,
+    duration: data.duration.toLowerCase(),
+    concentration: data.concentration,
+    time: data.casting_time,
+    school: data.school.toLowerCase(),
+    slug: data.slug
+  }
+}
 
 module.exports = router;
